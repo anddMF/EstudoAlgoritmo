c00412b92bb1e3cb5de7ff3bc3e3e6f03e43459d
Andrew Moraes <andrewmoraesds@gmail.com>
2021-11-23 16:18:24 -0300

    exercicio RepeatedNumber mas com O(n)

--- URIstudy/Program.cs
+++ URIstudy/Program.cs
@@ -25,7 +25,7 @@ namespace URIstudy
             //FirstDuplicated(numberList2);
             //PlusMinus(arr);
             //int result = WordLadder("hit", "cog", new List<string> { "hot", "dot", "dog", "lot", "log", "cog" });
-            var a = MaxSubArray();
+
             Console.ReadKey();
         }
 
@@ -1009,8 +1009,9 @@ namespace URIstudy
             return result;
         }
 
-        // E27 Kadane's Algorithm, encontrar a maior soma em uma subarray continua e retornar esse valor. Exemplo: input = [1,2,3,4,-10] vai
-        // ter como resposta o 10 porque 1+2+3+4 é a maior soma possível dentro do array.
+        // E27 Kadane's Algorithm, encontrar a maior soma em uma subarray continua e retornar esse valor.
+        //Exemplo: input = [1,2,3,4,-10] vai ter como resposta o 10 porque 1+2+3+4 é a maior soma possível
+        //dentro do array.
         public static int MaxSubArray(List<int> a)
         {
             //var A = new List<int> { 1, 2, 3, 4, -10 };
@@ -1019,8 +1020,8 @@ namespace URIstudy
 
             for(int i = 1; i < a.Count; i++)
             {
-                // currentMax assume o que é maior: a soma dos subarrays anteriores + o atual, ou somente o atual pois apenas um
-                // elemento também considerado subarray
+                // currentMax assume o que é maior: a soma dos subarrays anteriores + o atual,
+                //ou somente o atual pois apenas um elemento também considerado subarray
                 currentMax = System.Math.Max(a[i], currentMax + a[i]);
 
                 if (currentMax > totalMax)
@@ -1028,6 +1029,22 @@ namespace URIstudy
             }
 
             return totalMax;   
+        }
+
+        // E28 Retornar o duplicado na lista mas utilizando O(n) no tempo e traversing sequencial O(1)
+        public static int RepeatedNumber(List<int> A)
+        {
+            // Ordeno a lista para poder comparar com o anterior e não precisar de find ou outro for
+            A.Sort();
+            int previous = A[0];
+            for(int i = 1; i < A.Count; i++)
+            {
+                if (A[i] == previous)
+                    return A[i];
+
+                previous = A[i];
+            }
+            return -1;
         }
 
         public static ListNode RemoveNthFromEnd(int B)
